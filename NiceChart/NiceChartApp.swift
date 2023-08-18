17a263d76daa1508ae52c908f3814b372009f181
Yassine ElBadaoui <byassine52@users.noreply.github.com>
2023-08-19 08:54:43 +0900

    Initial Commit

---
+++ NiceChart/NiceChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  NiceChartApp.swift
+//  NiceChart
+//
+//  Created by Yassine ElBadaoui on 2023/08/19.
+//
+
+import SwiftUI
+
+@main
+struct NiceChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
